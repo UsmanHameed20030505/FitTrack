283b1f7c5bd505aec538d87552b37a72ea83e2a2
UsmanHameed20030505 <164863828+UsmanHameed20030505@users.noreply.github.com>
2024-06-29 13:58:44 +0500

    Create footer.php

---
+++ components/footer.php
@@ -0,0 +1,43 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <!-- Email contact box -->
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Our Email</h3>
+         <a href="mailto:usmanhameed@gmail.com">usmanhameed@gmail.com</a>
+      </div>
+
+      <!-- Opening hours box -->
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Opening Hours</h3>
+         <p>08:00AM Till 09:00PM</p>
+      </div>
+
+      <!-- Address box -->
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Our Address</h3>
+         <a href="#">Multan, Pakistan Zindabad</a>
+      </div>
+
+      <!-- Phone number box -->
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Our Number</h3>
+         <a href="tel:080078601">080078601</a>
+      </div>
+
+   </section>
+
+   <!-- Footer credit -->
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Mr. Usman Hameed</span> | all rights reserved!</div>
+
+</footer>
+
+<!-- Loader animation -->
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
