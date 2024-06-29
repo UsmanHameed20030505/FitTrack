59a2b08ffbebe372ff6e70394e3312bfcd57ff9e
UsmanHameed20030505 <164863828+UsmanHameed20030505@users.noreply.github.com>
2024-06-29 13:53:28 +0500

    Create user_header.php

---
+++ components/user_header.php
@@ -0,0 +1,79 @@
+<?php
+// Check if the $message variable is set
+if (isset($message)) {
+   // Loop through each message in the $message array
+   foreach ($message as $message) {
+      echo '
+      <div class="message">
+         <span>' . $message . '</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+<header class="header">
+
+   <section class="flex">
+
+      <!-- Logo section -->
+      <a href="home.php" class="logo">Usman Foods 😋</a>
+
+      <!-- Navigation bar -->
+      <nav class="navbar">
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="menu.php">Menu</a>
+         <a href="orders.php">Orders</a>
+         <a href="contact.php">Contact</a>
+      </nav>
+
+      <!-- Icons section -->
+      <div class="icons">
+         <?php
+            // Prepare and execute a query to count the items in the cart for the current user
+            $count_cart_items = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+            $count_cart_items->execute([$user_id]);
+            $total_cart_items = $count_cart_items->rowCount();
+         ?>
+         <!-- Display search, cart, user, and menu icons -->
+         <a href="search.php"><i class="fas fa-search"></i></a>
+         <a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(<?= $total_cart_items; ?>)</span></a>
+         <div id="user-btn" class="fas fa-user"></div>
+         <div id="menu-btn" class="fas fa-bars"></div>
+      </div>
+
+      <!-- Profile section -->
+      <div class="profile">
+         <?php
+            // Prepare and execute a query to get the user's profile information
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            if ($select_profile->rowCount() > 0) {
+               $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <!-- Display user's name and profile links if user is logged in -->
+         <p class="name"><?= $fetch_profile['name']; ?></p>
+         <div class="flex">
+            <a href="profile.php" class="btn">Profile</a>
+            <a href="components/user_logout.php" onclick="return confirm('logout from this website?');" class="delete-btn">Logout</a>
+         </div>
+         <p class="account">
+            <a href="login.php">Login</a> or
+            <a href="register.php">Register</a>
+         </p> 
+         <?php
+            } else {
+         ?>
+         <!-- Display login prompt if user is not logged in -->
+         <p class="name">Please Login First!</p>
+         <a href="login.php" class="btn">Login</a>
+         <?php
+            }
+         ?>
+      </div>
+
+   </section>
+
+</header>
